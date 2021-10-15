b57bce1940a1961b1a00f936d62769b35ad617ca
Basile Starynkevitch <basile@starynkevitch.net>
2021-10-15 20:56:00 +0200

    adding rps_show_version_info routine

--- main_rps.c
+++ main_rps.c
@@ -47,10 +47,46 @@ rps_backtrace_error_cb (void *data, const char *msg, int errnum)
   fprintf (stderr, "Refpersys Backtrace Error #%d: %s\n", errnum, msg);
 }
 
+void
+rps_show_version_info (int argc, char **argv)
+{
+  printf ("%s - a Reflexive Persistent System - see refpersys.org\n",
+	  rps_progname);
+  printf
+    ("... is an open source symbolic artificial intelligence project.\n");
+  printf ("\t email contact: <team@refpersys.org>\n");
+  printf ("\t build timestamp: %s (%ld)\n", rps_timestamp, rps_timelong);
+  printf ("\t top directory: %s\n", rps_topdirectory);
+  printf ("\t short git id: %s\n", rps_shortgitid);
+  printf ("\t full git id: %s\n", rps_gitid);
+  printf ("\t last git tag: %s\n", rps_lastgittag);
+  printf ("\t last git commit: %s\n", rps_lastgitcommit);
+  printf ("\t git remote origin URL: %s\n", rps_gitremoteoriginurl);
+  printf ("\t md5sum of files: %s\n", rps_md5sum);
+  printf ("\t build makefile: %s\n", rps_makefile);
+  printf ("\t built with compiler: %s\n", rps_c_compiler_version);
+  if (!rps_running_in_batch)
+    printf ("\t GTK version: %d.%d.%d (see gtk.org)\n",
+	    gtk_get_major_version (), gtk_get_minor_version (),
+	    gtk_get_micro_version ());
+  printf ("\t GNU libc version: %s (see www.gnu.org/software/libc)\n",
+	  gnu_get_libc_version ());
+  printf ("\t libcurl version: %s (see curl.se/libcurl)\n", curl_version ());
+  printf ("\t " __FILE__ " was compiled on " __DATE__ " at " __TIME__ "\n");
+  printf ("\t Subdirectories:\n");
+  for (int sdix = 0; rps_subdirectories[sdix] != NULL; sdix++)
+    printf ("\t   %s\n", rps_subdirectories[sdix]);
+  printf ("\t Files:\n");
+  for (int fix = 0; rps_files[fix] != NULL; fix++)
+    printf ("\t   %s\n", rps_files[fix]);
+}				/* end rps_show_version_info */
+
+
 int
 main (int argc, char **argv)
 {
   bool show_version_info = false;
+  bool show_help = false;
   rps_progname = argv[0];
   rps_backtrace_common_state =
     backtrace_create_state (rps_progname, (int) true,
@@ -72,43 +108,13 @@ main (int argc, char **argv)
 	rps_running_in_batch = true;
       else if (!strcmp (argv[aix], "--version"))
 	show_version_info = true;
+      else if (!strcmp (argv[aix], "--help"))
+	show_help = true;
     }
   pthread_setname_np (pthread_self (), "rps-main");
   if (!rps_running_in_batch)
     gtk_init (&argc, &argv);
   curl_global_init (CURL_GLOBAL_ALL);
   if (show_version_info)
-    {
-      printf ("%s - a Reflexive Persistent System - see refpersys.org\n",
-	      rps_progname);
-      printf
-	("... is an open source symbolic artificial intelligence project.\n");
-      printf ("\t email contact: <team@refpersys.org>\n");
-      printf ("\t build timestamp: %s (%ld)\n", rps_timestamp, rps_timelong);
-      printf ("\t top directory: %s\n", rps_topdirectory);
-      printf ("\t short git id: %s\n", rps_shortgitid);
-      printf ("\t full git id: %s\n", rps_gitid);
-      printf ("\t last git tag: %s\n", rps_lastgittag);
-      printf ("\t last git commit: %s\n", rps_lastgitcommit);
-      printf ("\t git remote origin URL: %s\n", rps_gitremoteoriginurl);
-      printf ("\t md5sum of files: %s\n", rps_md5sum);
-      printf ("\t build makefile: %s\n", rps_makefile);
-      printf ("\t built with compiler: %s\n", rps_c_compiler_version);
-      if (!rps_running_in_batch)
-	printf ("\t GTK version: %d.%d.%d (see gtk.org)\n",
-		gtk_get_major_version (), gtk_get_minor_version (),
-		gtk_get_micro_version ());
-      printf ("\t GNU libc version: %s (see www.gnu.org/software/libc)\n",
-	      gnu_get_libc_version ());
-      printf ("\t libcurl version: %s (see curl.se/libcurl)\n",
-	      curl_version ());
-      printf ("\t " __FILE__ " was compiled on " __DATE__ " at " __TIME__
-	      "\n");
-      printf ("\t Subdirectories:\n");
-      for (int sdix = 0; rps_subdirectories[sdix] != NULL; sdix++)
-	printf ("\t   %s\n", rps_subdirectories[sdix]);
-      printf ("\t Files:\n");
-      for (int fix = 0; rps_files[fix] != NULL; fix++)
-	printf ("\t   %s\n", rps_files[fix]);
-    }
+    rps_show_version_info (argc, argv);
 }				/* end of main function */
