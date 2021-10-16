9ff0bb85c34c13216fe0eb64fcebce7d0c7595da
Basile Starynkevitch <basile@starynkevitch.net>
2021-10-16 13:19:14 +0200

    implement rps_fatal_stop_at, rps_gettid, rps_clocktime, rps_abort

--- main_rps.c
+++ main_rps.c
@@ -47,7 +47,7 @@ GOptionEntry rps_gopt_entries[] = {
   {"batch", 'B', 0, G_OPTION_ARG_NONE, &rps_running_in_batch,
    "run in batch mode, without user interface", NULL},
   {"version", 0, 0, G_OPTION_ARG_NONE, &rps_showing_version,
-   "show version information", NULL},
+   "show version information and default options", NULL},
   {NULL}
 };
 
@@ -108,6 +108,40 @@ rps_show_version_info (int argc, char **argv)
 	    uts.sysname, uts.release, uts.version);
 }				/* end rps_show_version_info */
 
+void
+rps_fatal_stop_at (const char *fil, int lineno)
+{
+  char thnambuf[16];
+  memset (thnambuf, 0, sizeof (thnambuf));
+  pthread_getname_np (pthread_self (), thnambuf, sizeof (thnambuf));
+  fprintf (stderr, "** FATAL STOP %s:%d (tid#%d/%s)\n",
+           fil ? fil : "???", lineno, (int) rps_gettid (), thnambuf);
+  fflush (stderr);
+  rps_abort ();
+}                               /* end rps_fatal_stop */
+
+pid_t
+rps_gettid (void)
+{
+  return syscall (SYS_gettid, 0L);
+}                               /* end rps_gettid */
+
+
+double
+rps_clocktime (clockid_t clid)
+{
+  struct timespec ts = { 0, 0 };
+  if (clock_gettime (clid, &ts))
+    return NAN;
+  return (double) ts.tv_sec + 1.0e-9 * ts.tv_nsec;
+}
+
+void
+rps_abort(void)
+{
+  abort();
+} /* end rps_abort */
+
 int
 main (int argc, char **argv)
 {
@@ -145,7 +179,10 @@ main (int argc, char **argv)
     {
       rps_show_version_info (argc, argv);
       exit (EXIT_SUCCESS);
-    }
+    };
+  if (!rps_load_directory)
+    rps_load_directory = rps_topdirectory;
+  rps_load_initial_heap();
 }				/* end of main function */
 
 
