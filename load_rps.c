39be94f9a18e8bb5855a4cce46c4050b8e3beb8d
Basile Starynkevitch <basile@starynkevitch.net>
2021-10-18 07:22:02 +0200

    add RPSLOADING_PARSE_MANIFEST_PASS, and started coding incomplete rps_load_parse_manifest

--- load_rps.c
+++ load_rps.c
@@ -38,6 +38,7 @@
 enum rps_loaderstate_en
 {
   RPSLOADING__NONE,
+  RPSLOADING_PARSE_MANIFEST_PASS,
   RPSLOADING_CREATE_OBJECTS_PASS,
   RPSLOADING_FILL_OBJECTS_PASS,
   RPSLOADING_EPILOGUE_PASS,
@@ -49,6 +50,8 @@ struct RpsPayl_Loader_st
   RPSFIELDS_ZONED_VALUE;
   unsigned ld_magic;		/* always RPS_LOADER_MAGIC */
   enum rps_loaderstate_en ld_state;
+  FILE *ld_manifest_file;
+  json_t *ld_json_manifest;
 };
 
 bool
@@ -57,15 +60,64 @@ rps_is_valid_loader (RpsLoader_t * ld)
   if (!ld)
     return false;
   if (ld->ld_magic == RPS_LOADER_MAGIC)
-    return ld->ld_state == RPSLOADING_CREATE_OBJECTS_PASS
+    return ld->ld_state == RPSLOADING_PARSE_MANIFEST_PASS
+      || ld->ld_state == RPSLOADING_CREATE_OBJECTS_PASS
       || ld->ld_state == RPSLOADING_FILL_OBJECTS_PASS
       || ld->ld_state == RPSLOADING_EPILOGUE_PASS;
   return false;
 }				/* end rps_is_valid_loader */
 
+
+void
+rps_load_parse_manifest (RpsLoader_t * ld)
+{
+  int linenum = 0;
+  char manifestpath[256];
+  memset (manifestpath, 0, sizeof (manifestpath));
+  if (!rps_is_valid_loader (ld))
+    RPS_FATAL ("invalid loader %p to rps_load_parse_manifest", ld);
+  snprintf (manifestpath, sizeof (manifestpath),
+	    "%d/rps_manifest.json", rps_load_directory);
+  ld->ld_manifest_file = fopen (manifestpath, "r");
+  if (!ld->ld_manifest_file)
+    RPS_FATAL ("missing loader manifest file %s - %m", manifestpath);
+  /// skip initial comment lines in manifest
+  {
+    char linbuf[256];
+    long linoff = -1;
+    while (memset (linbuf, 0, sizeof (linbuf)),
+	   (linoff = ftell (ld->ld_manifest_file)),
+	   fgets (linbuf, sizeof (linbuf), ld->ld_manifest_file))
+      {
+	linenum++;
+	if (linbuf[0] != '/')
+	  break;
+      }
+    fseek (ld->ld_manifest_file, linoff, SEEK_SET);
+  }
+  /// parse the JSON of the manifest
+  {
+    json_error_t jerr = { };
+    ld->ld_json_manifest
+      = json_loadf (ld->ld_manifest_file,
+		    JSON_REJECT_DUPLICATES | JSON_DISABLE_EOF_CHECK, &jerr);
+    if (!ld->ld_json_manifest)
+      RPS_FATAL
+	("failed to parse JSON in manifest file %s line %d - %s @%sL%dC%d",
+	 manifestpath, linenum, jerr.text, jerr.source, jerr.line,
+	 jerr.column);
+  }
+#warning missing code using the JSON manifest in rps_load_parse_manifest
+  fclose (ld->ld_manifest_file), ld->ld_manifest_file = NULL;
+}				/* end rps_load_parse_manifest */
+
 void
 rps_load_initial_heap (void)
 {
+  RpsLoader_t *loader = RPS_ALLOC_ZONE (sizeof (RpsLoader_t), RpsPyt_Loader);
+  loader->ld_magic = RPS_LOADER_MAGIC;
+  loader->ld_state = RPSLOADING_PARSE_MANIFEST_PASS;
+  rps_load_parse_manifest (loader);
 #warning rps_load_initial_heap needs to be coded
   RPS_FATAL ("unimplemented rps_load_initial_heap load directory %s",
 	     rps_load_directory);
