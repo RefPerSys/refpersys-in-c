752fcf9c6d1e820ccd5297de7160491d14030cf7
Basile Starynkevitch <basile@starynkevitch.net>
2021-11-03 06:00:38 +0100

    more debug printing in rps_load_first_pass

--- src/load_rps.c
+++ src/load_rps.c
@@ -349,8 +349,9 @@ rps_load_first_pass (RpsLoader_t * ld, int spix, RpsOid spaceid)
 	{
 	  rps_check_all_objects_buckets_are_valid ();
 	  if (objcount % 16 == 0)
-	    printf ("rps_load_first_pass space#%d objcount %ld file %s:%d (%s:%d)\n",
-		    spix, objcount, filepath, lincnt, __FILE__, __LINE__);
+	    printf
+	      ("rps_load_first_pass space#%d objcount %ld file %s:%d (%s:%d)\n",
+	       spix, objcount, filepath, lincnt, __FILE__, __LINE__);
 	};
       memset (linbuf, 0, linsz);
       linoff = ftell (spfil);
@@ -430,8 +431,8 @@ rps_load_first_pass (RpsLoader_t * ld, int spix, RpsOid spaceid)
 	  }
       }
     }
-  printf ("rps_load_first_pass created %ld objects at %s:%d (%s:%d)\n", objcount,
-	  filepath, lincnt, __FILE__, __LINE__);
+  printf ("rps_load_first_pass created %ld objects at %s:%d (%s:%d)\n",
+	  objcount, filepath, lincnt, __FILE__, __LINE__);
   fclose (spfil);
   rps_check_all_objects_buckets_are_valid ();
 }				/* end rps_load_first_pass */
