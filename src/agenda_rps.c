6a76e0f6b4b0fb6c2cb4f64d0a97c7d09460a712
Basile Starynkevitch <basile@starynkevitch.net>
2021-11-15 12:51:39 +0100

    adding with incomplete stub for rpsldpy_agenda; missing agenda payload

---
+++ src/agenda_rps.c
@@ -0,0 +1,46 @@
+/****************************************************************
+ * file agenda_rps.c
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Description:
+ *      This file is part of the Reflective Persistent System.
+ *
+ *      Implementation of the agenda
+ *
+ *
+ *      © Copyright 2019 - 2021 The Reflective Persistent System Team
+ *      team@refpersys.org & http://refpersys.org/
+ *
+ * License:
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+#include "Refpersys.h"
+
+void
+rpsldpy_agenda (RpsObject_t * obj, RpsLoader_t * ld, const json_t * jv,
+		int spix)
+{
+  char idbuf[32];
+  memset (idbuf, 0, sizeof (idbuf));
+  RPS_ASSERT (obj != NULL);
+  RPS_ASSERT (rps_is_valid_object (obj));
+  rps_oid_to_cbuf (obj->ob_id, idbuf);
+  RPS_ASSERT (rps_is_valid_filling_loader (ld));
+  RPS_ASSERT (json_is_object (jv));
+  RPS_ASSERT (spix > 0);
+#warning rpsldpy_agenda unimplemented
+  RPS_FATAL ("unimplemented rpsldpy_agenda obj %s spix#%d\n..jv=%s",
+	     idbuf, spix, json_dumps (jv, JSON_INDENT (2) | JSON_SORT_KEYS));
+}				/* end rpsldpy_agenda */
