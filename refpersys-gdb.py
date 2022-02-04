da33d153c57edc960a1f6d178c2a82abdf09be01
Basile Starynkevitch <basile@starynkevitch.net>
2022-02-04 20:00:28 +0100

    non-working Python script to help debug refpersys (in C)

---
+++ refpersys-gdb.py
@@ -0,0 +1,7 @@
+# -*- python -*-
+# SPDX-License-Identifier: GPL-3.0-or-later
+## this is file refpersys-gdb.py of RefPerSys (in C) - see refpersys.org
+gdb.write ("Starting GDB python script file refpersys-gdb.py\n")
+### sadly this does not work yet....
+#- gdb.write ("refpersys [in C] git " + gdb.parse_and_eval("_rps_git_short_id").dereference())
+
