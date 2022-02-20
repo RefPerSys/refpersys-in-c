75002a358f5ad02de4830f9c195fc3826b014539
Basile Starynkevitch <basile@starynkevitch.net>
2022-02-20 08:59:58 +0100

    better rps_verify_locked_object_payload and checking class in rps_is_valid_object

--- src/object_rps.c
+++ src/object_rps.c
@@ -38,7 +38,8 @@ rps_is_valid_object (RpsObject_t * obj)
   if (obj->ob_magic != RPS_OBJ_MAGIC)
     return false;
   RPS_ASSERT (rps_oid_is_valid (obj->ob_id));
-  RPS_ASSERT (obj->ob_class != NULL);
+  RPS_ASSERTPRINTF (obj->ob_class != NULL,
+		    "object %O without class", obj);
 //- pthread_mutex_lock (&obj->ob_mtx);
 //- if (obj->ob_class == NULL)
 //-   {
@@ -1205,7 +1206,7 @@ rps_verify_locked_object_payload (RpsObject_t * ob, int paylty, void *payl)
   if (paylty == 0 || paylty >= RPS_MAX_PAYLOAD_TYPE_INDEX)
     RPS_FATAL
       ("payload type#%d invalid for object %O payl@%p", paylty, ob, payl);
-  RPS_ASSERT (atomic_load (&ob->zm_atype) == paylty);
+  RPS_ASSERT (RPS_ZONED_MEMORY_TYPE(payl) == paylty);
   pthread_mutex_lock (&rps_payload_mtx);
   verifrout = rps_payload_verifying_rout_arr[-paylty];
   verifdata = rps_payload_verifying_data_arr[-paylty];
